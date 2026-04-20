2dc0fcf8049dc6ec8d70b6ab69019a5b634c10a1
Atharva Rai <atharvarai07@gmail.com>
2026-04-20 21:11:23 +0530

    Completed Data Ingestion

---
+++ config/paths_config.py
@@ -0,0 +1,7 @@
+import os
+
+###################################### DATA INGESTION RELATED PATHS ######################################
+RAW_DATA_DIR = os.path.join("artifacts", "raw_data")
+RAW_DATA_FILE_PATH = os.path.join(RAW_DATA_DIR, "data.csv")
+
+CONFIG_PATH = os.path.join("config", "config.yaml")
